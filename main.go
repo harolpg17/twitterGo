04f296c4b75d7b8a565f204184334509af01dcf9
harolpg17 <harolpg17@gmail.com>
2025-03-14 17:21:25 -0500

    Último Commit

--- main.go
+++ main.go
@@ -47,7 +47,9 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 
-	fmt.Println("Path: " + request.PathParameters["twittergo"])
+	fmt.Println("Path twittergo: " + request.PathParameters["twittergo"])
+	fmt.Println("Path twitter: " + request.PathParameters["twitter"])
+	fmt.Println("Path twitterGo: " + request.PathParameters["twitterGo"])
 	fmt.Println("UrlPrefix: " + os.Getenv("UrlPrefix"))
 
 	path := strings.Replace(request.PathParameters["twittergo"], os.Getenv("UrlPrefix"), "", -1)
